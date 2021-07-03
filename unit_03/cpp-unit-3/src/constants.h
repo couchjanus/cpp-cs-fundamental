e41f7aa6b36b63ffb9691384ecd4ed73eec04257
couchjanus <couchjanus@gmail.com>
2021-07-03 16:33:30 +0300

    added unit 3

---
+++ unit_03/cpp-unit-3/src/constants.h
@@ -0,0 +1,16 @@
+/*
+ * constants.h
+ *
+ *  Created on: 3 июл. 2021 г.
+ *      Author: janus
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+namespace constants
+{
+	const double PI(3.14159);
+	const double AVOGADRO(6.0221413e23);
+	const double GRAVITY(9.2);
+}
+#endif /* CONSTANTS_H_ */
